9edd4c919bd9c7cba9134d026cf7bd73adac3e3a
MarikIshtar007 <asterp04@gmail.com>
2020-06-29 19:31:25 +0530

    Refactoring and UI Changes.

--- final_tic.py
+++ final_tic.py
@@ -1,7 +1,6 @@
 from math import inf
 import time
 from random import randint
-from colored_io import *
 
 recur_call = 0
 
@@ -30,16 +29,16 @@ def show_board(board, player_key, comp_key):
             elif board[i][j] == -1:
                 print(f"\033[96m {play_dico[board[i][j]]}\033[00m".center(16), end="")
             if j != 2:
-                print("\033[93m {}\033[00m".format('|'), end="")
+                print(f"\033[93m {'|'}\033[00m", end="")
             number += 1
         print()
         if i != 2:
-            prYellow("---------------------", endo="\n")
+            print(f"\033[93m {'---------------------'}\033[00m")
     print("")
 
 
 def win(board, pl):
-    winnin_pos = [
+    winning_positions = [
         [(0, 0), (0, 1), (0, 2)],
         [(1, 0), (1, 1), (1, 2)],
         [(2, 0), (2, 1), (2, 2)],
@@ -49,7 +48,7 @@ def win(board, pl):
         [(0, 0), (1, 1), (2, 2)],
         [(0, 2), (1, 1), (2, 0)]
     ]
-    for posi in winnin_pos:
+    for posi in winning_positions:
         temp = []
         for position in posi:
             x, y = position
@@ -59,15 +58,6 @@ def win(board, pl):
     return False
 
 
-# def is_empty(board):
-#     empty_places = []
-#
-#     for i, row in enumerate(board):
-#         for j, cell in enumerate(row):
-#             if cell == 0:
-#                 empty_places.append([i, j])
-#     return empty_places
-
 def get_depth(board):
     depth = 0
     for i in range(3):
@@ -75,6 +65,7 @@ def get_depth(board):
             if board[i][j] == 0:
                 depth += 1
     return depth
+
 
 def evaluate(board):
     if win(board, comp):
@@ -128,7 +119,7 @@ def comp_turn(player_key, comp_key):
     depth = get_depth(board)
     if depth == 0 or win(board, human) or win(board, comp):
         return
-    print("It's the Computer's turn...\n")
+    print("WAIT!!\nIt's the Computer's turn...\n")
     time.sleep(1.4)
     if depth == 9:
         i = randint(0, 2)
@@ -160,7 +151,7 @@ def player_turn(player_key, comp_key):
     while loc not in dico:
         loc = input("Enter proper cell number\n")
     x, y = dico[loc]
-    if board[x][y]== 0:
+    if board[x][y] == 0:
         board[x][y] = human
     else:
         print("That's an illegal move. You lost your turn.")
@@ -168,55 +159,82 @@ def player_turn(player_key, comp_key):
     show_board(board, player_key, comp_key)
 
 
+def reset():
+    global board
+    global recur_call
+    board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+    recur_call = 0
+    print("Everything has been cleared. Get ready for another bout!")
+
+
 def main():
+    replay = 'y'
     print("Welcome to the Game of Tic Tac Toe.. played by a Machine!!")
     print("Can you Beat it?.. ")
-    time.sleep(1.6)
-    player_key = input("\n\n Would you like to be X or O ?\n\n").upper()
-    while player_key.lower() != 'x' and player_key.lower() != 'o':
-        player_key = input("Please enter a valid choice. Either X or O\n").upper()
-    if player_key == 'X':
-        comp_key = 'O'
-        comp1 = False
-    else:
-        comp_key = 'X'
-        comp1 = True
-    print(f'You have chosen {player_key}.')
-    time.sleep(2)
-    print(f'The Computer gets the {comp_key}.')
-    time.sleep(2)
-    print("As a rule.... X always goes first..")
-
-    # players = itertools.cycle(['X', 'O'])
-    # if player == 'O':
-    #     curr_player = next(players)
-    #     curr_player = next(players)
-    # else:
-    #     curr_player = next(players)
-    #     print("the current player is ", curr_player)
-    # show_board(board)
-    # print("The Grid locations are  1 - 9.")
-    # print("Enter the grid location to place your", curr_player)
-
-    # Actual game loop starts
-
-    # Actual Game loop
-    show_board(board, player_key, comp_key)
-    while get_depth(board) != 0:
-        if comp1:
-            comp_turn(player_key=player_key, comp_key=comp_key)
+    while replay == 'y':
+        time.sleep(1.6)
+        player_key = input("\n Would you like to be X or O ?\n\n").upper()
+        while player_key.lower() != 'x' and player_key.lower() != 'o':
+            player_key = input("Please enter a valid choice. Either X or O\n").upper()
+        if player_key == 'X':
+            comp_key = 'O'
             comp1 = False
-        player_turn(player_key, comp_key)
-        comp_turn(player_key, comp_key)
-        if win(board, comp) or win(board, human):
-            break
+        else:
+            comp_key = 'X'
+            comp1 = True
+        print(f'You have chosen {player_key}.')
+        time.sleep(2)
+        print(f'The Computer gets the {comp_key}.')
+        time.sleep(2)
+        print("As a rule.... X always goes first..")
 
-    if win(board, human):
-        print("You Won")
-    elif win(board, comp):
-        print("You LOST!")
-    else:
-        print("It's a draw")
+        # players = itertools.cycle(['X', 'O'])
+        # if player == 'O':
+        #     curr_player = next(players)
+        #     curr_player = next(players)
+        # else:
+        #     curr_player = next(players)
+        #     print("the current player is ", curr_player)
+        # show_board(board)
+        # print("The Grid locations are  1 - 9.")
+        # print("Enter the grid location to place your", curr_player)
+
+        # Actual game loop starts
+
+        # Actual Game loop
+        show_board(board, player_key, comp_key)
+        while get_depth(board) != 0:
+            if comp1:
+                comp_turn(player_key=player_key, comp_key=comp_key)
+                comp1 = False
+            player_turn(player_key, comp_key)
+            comp_turn(player_key, comp_key)
+            if win(board, comp) or win(board, human):
+                break
+        time.sleep(0.8)
+
+        if win(board, human):
+            print(f"\033[91m {'You Won!!!! Congrats.'}\033[00m")
+        elif win(board, comp):
+            print(f"\033[96m {'You Lost...'}\033[00m")
+            time.sleep(1)
+            print(f"\033[96m {'... at Tic-Tac-Toe...'}\033[00m")
+            time.sleep(1)
+            print(f"\033[96m {'Against a Non-ML Script. SHEESH!!'}\033[00m")
+            time.sleep(1)
+        else:
+            print("\033[93m {}\033[00m".format('That\'s a Draw!!!'))
+            print(f"\033[93m {'----------------------------------------------------------------'}\033[00m")
+
+        print("Would you like to play again..?")
+        time.sleep(1)
+        replay = input("Enter y OR n\n")
+        if replay == 'y':
+            reset()
     exit()
 
 
